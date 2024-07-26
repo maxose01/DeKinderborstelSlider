8942187d654cadf2686ec81e96829613a27457f1
Max Osewoudt <45993958+maxose01@users.noreply.github.com>
2024-07-26 21:36:45 +0200

    Revert "Improved rotation mini cloud"
    This reverts commit ecbd8a673d60e534eff2255fae6b4fe604f3a249.

--- mobile-script.js
+++ mobile-script.js
@@ -1,42 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     let currentIndex = 0;
-    let previousIndex = -1;
     const frames = document.querySelectorAll('.frame');
     const totalFrames = frames.length;
-    const cloudText = document.getElementById('cloud-text');
-    const minicloud = document.querySelector('.minicloud');
-    const colors = ['#FFF7D3', '#D7F0FF', '#FFEBF2'];
-    const texts = ['Speels karakter', 'Frisse kleur', 'Zacht gevoel'];
-
-    function getRandomIndex() {
-        let randomIndex = Math.floor(Math.random() * colors.length);
-        while (randomIndex === previousIndex) {
-            randomIndex = Math.floor(Math.random() * colors.length);
-        }
-        previousIndex = randomIndex;
-        return randomIndex;
-    }
 
     function showFrame(index) {
         frames.forEach((frame, i) => {
             frame.classList.toggle('active', i === index);
         });
-    }
-
-    function updateCloud() {
-        const randomIndex = getRandomIndex();
-        minicloud.style.backgroundColor = colors[randomIndex];
-        cloudText.textContent = texts[randomIndex];
-        document.styleSheets[0].addRule('.minicloud:before', `background-color: ${colors[randomIndex]} !important`);
-        document.styleSheets[0].addRule('.minicloud:after', `background-color: ${colors[randomIndex]} !important`);
-    }
-
-    function fadeCloud() {
-        minicloud.style.opacity = 0; // Fade out
-        setTimeout(() => {
-            updateCloud(); // Update color and text while invisible
-            minicloud.style.opacity = 1; // Fade in
-        }, 1000); // Wait for fade out to complete before updating
     }
 
     function nextFrame() {
@@ -45,7 +15,6 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     setInterval(nextFrame, 4000);
-    setInterval(fadeCloud, 5000); // Change cloud color and text every 5 seconds
 
     let startX = 0;
     let endX = 0;
@@ -60,11 +29,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
     function handleTouchEnd() {
         if (startX > endX + 50) {
-            nextFrame();
+            currentIndex = (currentIndex + 1) % totalFrames;
         } else if (startX < endX - 50) {
             currentIndex = (currentIndex - 1 + totalFrames) % totalFrames;
-            showFrame(currentIndex);
         }
+        showFrame(currentIndex);
     }
 
     document.addEventListener('touchstart', handleTouchStart);
